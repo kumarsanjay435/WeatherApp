71d8eb83a1640de8b35d3c1c5330bc3acf1bee90
kumarsanjay435 <kumarsanjay435@gmail.com>
2024-06-08 10:03:15 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Sanjay Kumar on 2024-06-08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
